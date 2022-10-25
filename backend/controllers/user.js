384fadfb69cba5c909ec3beec639b925b3cec96f
Patricia <patricia.allande@gmail.com>
2022-10-24 23:32:58 -0400

    added multer

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -2,15 +2,18 @@ const bcrypt = require('bcrypt');
 const { use } = require('../app');
 const { validate } = require('../models/user');
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+
+console.log('first hello');
 
 exports.signup = (req, res, next) => {
-  console.log('hello');
-    bcrypt.hash(req.password, 10).then(
+  console.log(req.password);
+    bcrypt.hash(req.body.password, 10).then(
         (hash) => {
             const user = new User({
                 email: req.body.email,
                 password: hash
-            });
+            }); console.log('why');
             user.save().then(
                 () => {
                     res.status(201).json({
@@ -43,9 +46,13 @@ exports.login = (req, res, next) => {
                 error: new Error('Incorrect password!')
               });
             }
+            const token = jwt.sign(
+              { userId: user._id },
+              'RANDOM_TOKEN_SECRET',
+              { expiresIn: '24h' });
             res.status(200).json({
               userId: user._id,
-              token: 'Bearer <token>'
+              token: token
             });
           }
         ).catch(
